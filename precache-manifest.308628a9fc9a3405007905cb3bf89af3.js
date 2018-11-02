51985e5b75464ab9b75564f79d4f135dc4d3f3e9
minheinkhant <aungkoung007@gmail.com>
2018-11-03 05:28:23 +0630

    Updates

---
+++ precache-manifest.308628a9fc9a3405007905cb3bf89af3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a8c39b682b050caf2e4",
+    "url": "/report/static/js/runtime~main.8a8c39b6.js"
+  },
+  {
+    "revision": "92372e8eff89610a480a",
+    "url": "/report/static/js/main.92372e8e.chunk.js"
+  },
+  {
+    "revision": "f455a9d241954420920e",
+    "url": "/report/static/js/1.f455a9d2.chunk.js"
+  },
+  {
+    "revision": "92372e8eff89610a480a",
+    "url": "/report/static/css/main.8dc8bd9a.chunk.css"
+  },
+  {
+    "revision": "e7e6523c8e1c1117616c613649d4ca55",
+    "url": "/report/index.html"
+  }
+];
